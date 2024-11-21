d95da377a58398b1319bed1102ff14de5c920c86
MinThantMaung <minthant180@gmail.com>
2024-11-21 15:34:51 +0900

    feat:create static data

---
+++ src/type/index.ts
@@ -0,0 +1,35 @@
+export const categories = [
+    {
+        name : "Sci-Fi"
+    },
+    {
+        name : "Action"
+    },
+    {
+        name : "Crime"
+    },
+    {
+        name : "Drama"
+    },
+    {
+        name : "Biography"
+    },
+    {
+        name : "History"
+    },
+    {
+        name : "Romance"
+    },
+    {
+        name : "Adventure"
+    },
+    {
+        name : "Thriller"
+    },
+    {
+        name : "War"
+    },
+    {
+        name : "Animation"
+    },
+]
