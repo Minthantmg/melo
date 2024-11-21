51e16caa19640425b4d1e19ae690e24d4b1a1a38
MinThantMaung <minthant180@gmail.com>
2024-11-21 15:33:56 +0900

    feat:create feature component and update ui

---
+++ src/app/components/feature.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Feature = () => {
+    return (
+        <div className='md:flex md:h-60 mt-10'>
+            <div className='w-5/12 bg-red-300 rounded-2xl'>
+                hello
+            </div>
+            <div className='w-7/12 bg-blue-300 ml-6 rounded-2xl'>
+                world
+            </div>
+        </div>
+    );
+};
+
+export default Feature;
