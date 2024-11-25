f7421af2101b9990af309893dc786cad7c96b3f8
MinThantMaung <minthant180@gmail.com>
2024-11-25 11:58:52 +0900

    fix:update ui

--- src/app/components/feature.tsx
+++ src/app/components/feature.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Feature = () => {
     return (
-        <div className='md:flex md:h-60 mt-10'>
-            <div className='w-5/12 bg-red-300 rounded-2xl'>
-                hello
+        <div className='md:flex h-32 md:h-60 mt-10'>
+            <div className='md:w-5/12 bg-red-300 rounded-2xl h-full'>
             </div>
-            <div className='w-7/12 bg-blue-300 ml-6 rounded-2xl'>
-                world
+            <div className='md:w-7/12 bg-blue-300 md:ml-6 rounded-2xl h-full hidden md:block'>
             </div>
         </div>
     );
