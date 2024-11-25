4cadfd4ba01488e86affa6aaae7ced158c8a940d
MinThantMaung <minthant180@gmail.com>
2024-11-25 11:59:49 +0900

    feat:add demo data

--- src/type/index.ts
+++ src/type/index.ts
@@ -33,3 +33,46 @@ export const categories = [
         name : "Animation"
     },
 ]
+
+export const demo_movies = [
+    {
+        name : "loetoeng kasarung",
+        rating : "7.8",
+        releaseDate : "2023"
+    },
+    {
+        name : "Gajah Langka",
+        rating : "6.0",
+        releaseDate : "2023"
+    },
+    {
+        name : "Si Kang Satay",
+        rating : "7.1",
+        releaseDate : "2023"
+    },
+    {
+        name : "Memory Cat",
+        rating : "7.8",
+        releaseDate : "2023"
+    },
+    {
+        name : "Hijabar Cantiq",
+        rating : "6.1",
+        releaseDate : "2023"
+    },
+    {
+        name : "Xatra- X",
+        rating : "6.5",
+        releaseDate : "2022"
+    },
+    {
+        name : "Hijabar Cantiq",
+        rating : "6.1",
+        releaseDate : "2023"
+    },
+    {
+        name : "Memory Cat",
+        rating : "7.8",
+        releaseDate : "2023"
+    },
+]
