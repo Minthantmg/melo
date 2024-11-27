d0e05724cb62636dfb344916dd8b5b2d415d0741
MinThantMaung <minthant180@gmail.com>
2024-11-27 14:59:16 +0900

    feat:bind data in ui

--- src/app/components/movies.tsx
+++ src/app/components/movies.tsx
@@ -1,44 +1,74 @@
 'use client'
 import React from 'react';
 import {Carousel, CarouselContent, CarouselItem} from "@/components/ui/carousel";
-import {demo_movies} from "@/type";
 import {Separator} from "@/components/ui/separator";
 import {useMovies} from "../../../hooks/useMovies";
 
+interface Comment {
+    "username": string,
+    "commentText": string,
+    "rating": number,
+    "createdAt": string,
+    "_id": string
+}
+
+interface Movie {
+    "_id": string,
+    "title": string,
+    "description": string,
+    "releaseDate": string,
+    "genre":string[],
+    "rating": number,
+    "director": string,
+    "cast": string[],
+    "duration": number,
+    "createdAt": string,
+    "__v": number,
+    "comments": Comment[]
+}
+
 const Movies = () => {
     const {getMoviesListHook} = useMovies()
-    const {data: moviesData} = getMoviesListHook()
+    const {data: moviesData,isLoading,isSuccess,isError} = getMoviesListHook()
+    const formattedDate = new Intl.DateTimeFormat('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    }).format(moviesData?.releaseDate);
 
-    console.log(moviesData)
     return (
         <div>
             <div className='mt-8 font-bold text-2xl'>
                 Currently Trending
             </div>
-            <div>
-                <Carousel className="mt-10">
-                    <CarouselContent>
-                        {demo_movies.map((movie, index) => (
-                            <CarouselItem key={index} className='md:basis-1/6 basis-1/2'>
-                                <div className='md:h-52 rounded-3xl bg-gray-300'>
-                                </div>
-                                <div className='mt-4 text-md'>
-                                    {movie.name}
-                                </div>
-                                <div className="flex h-5 items-center space-x-2 text-sm mt-2 mb-4">
-                                    <div>
-                                        {movie.rating}
+            {isLoading && (<>Loading...</>)}
+            {isError && (<>Error...</>)}
+            {isSuccess && (
+                <>
+                    <Carousel className="mt-10">
+                        <CarouselContent>
+                            {moviesData.map((movie: Movie, index: number) => (
+                                <CarouselItem key={index} className='md:basis-1/6 basis-1/2'>
+                                    <div className='md:h-52 rounded-3xl bg-gray-300'>
                                     </div>
-                                    <Separator orientation="vertical"/>
-                                    <div>
-                                        {movie.releaseDate}
+                                    <div className='mt-4 text-md select-none'>
+                                        {movie.title}
                                     </div>
-                                </div>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                </Carousel>
-            </div>
+                                    <div className="flex h-5 items-center space-x-2 text-sm mt-2 mb-4 select-none">
+                                        <div>
+                                            {movie.rating}
+                                        </div>
+                                        <Separator orientation="vertical"/>
+                                        <div className='select-none'>
+                                            {formattedDate}
+                                        </div>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                    </Carousel>
+                </>
+            )}
         </div>
     );
 };
