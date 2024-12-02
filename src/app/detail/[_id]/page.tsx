b3e87c94041c0a21e594946b61c4b98f248e2fb9
MinThantMaung <minthant180@gmail.com>
2024-12-02 17:22:22 +0900

    fix:update ui

--- src/app/detail/[_id]/page.tsx
+++ src/app/detail/[_id]/page.tsx
@@ -2,13 +2,21 @@
 import React from 'react';
 import {useParams} from "next/navigation";
 import {useMovies} from "../../../../hooks/useMovies";
+import {CalendarDays,Tag,Clock} from 'lucide-react'
 
 const Page = () => {
     const {_id} = useParams();
     const id = _id as string;
     const {getMovieByIdHook} = useMovies()
     const { data: movieData,isSuccess,isLoading,isError } = getMovieByIdHook(id);
-    console.log(movieData)
+
+    const formattedDate = movieData?.releaseDate
+        ? new Intl.DateTimeFormat('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+        }).format(new Date(movieData.releaseDate))
+        : 'Unknown Date';
 
     return (
         <>
@@ -17,37 +25,57 @@ const Page = () => {
             {isSuccess && (
                 <>
                     <div className='absolute h-[250px] w-full bg-gray-300'></div>
-                    <div className='md:mx-60'>
-                        <div className='relative pt-44'>
+                    <div className='md:mx-60 mx-4'>
+                        <div className='relative pt-36 md:pt-44'>
                             <div className='flex'>
-                                <div className='h-52 bg-red-300 w-1/5'>
+                                <div className='h-64 md:h-52 bg-red-300 w-1/3 md:w-1/5'>
                                 </div>
-                                <div className='w-4/5'>
-                                    <div className='flex justify-between'>
+                                <div className='w-2/3 md:w-4/5'>
+                                    <div className='flex justify-between ml-4'>
                                         <div>
-                                            <div>
-                                                This is title
+                                            <div className='text-xl font-sans font-bold'>
+                                                {movieData.title}
                                             </div>
-                                            <div>
-                                                this is star
-                                            </div>
-                                            <div>
-                                                this is date box
+                                            <div className='md:flex md:mt-3'>
+                                                <div className='flex text-xs mt-2 md:mt-1'>
+                                                    <div>
+                                                        <CalendarDays width={15} height={15}/>
+                                                    </div>
+                                                    <div className='flex justify-center items-center ml-1'>
+                                                        {formattedDate}
+                                                    </div>
+                                                </div>
+                                                <div className='flex text-xs mt-1 md:ml-4'>
+                                                    <div>
+                                                        <Tag width={15} height={15}/>
+                                                    </div>
+                                                    <div className='flex justify-center items-center ml-1'>
+                                                        {movieData.genre}
+                                                    </div>
+                                                </div>
+                                                <div className='flex text-xs mt-1 md:ml-4'>
+                                                    <div>
+                                                        <Clock width={15} height={15}/>
+                                                    </div>
+                                                    <div className='flex justify-center items-center ml-1'>
+                                                        {movieData.duration} min
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                         <div>
                                             this is button
                                         </div>
                                     </div>
-                                    <div>
-                                        <div>
-                                            this is synopsis
+                                    <div className='mt-6 ml-4'>
+                                    <div className='text-lg font-sans font-bold text-gray-500'>
+                                            Synopsis
                                         </div>
-                                        <div>
-                                            this is detail
+                                        <div className='mt-4 text-sm line-clamp-3'>
+                                            {movieData.description}
                                         </div>
-                                        <div>
-                                            this is director group
+                                        <div className='mt-3 text-sm'>
+                                            {movieData.director}
                                         </div>
                                     </div>
                                 </div>
