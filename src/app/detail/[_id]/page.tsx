fd59016009a129e4b5594940660eb8865d2d097f
MinThantMaung <minthant180@gmail.com>
2024-12-02 14:38:42 +0900

    fix:update dummy data for ui testing

--- src/app/detail/[_id]/page.tsx
+++ src/app/detail/[_id]/page.tsx
@@ -1,31 +1,60 @@
 'use client'
 import React from 'react';
-import {useParams, useRouter} from "next/navigation";
-import {MoveLeft} from 'lucide-react'
+import {useParams} from "next/navigation";
 import {useMovies} from "../../../../hooks/useMovies";
 
 const Page = () => {
-    const router = useRouter();
     const {_id} = useParams();
     const id = _id as string;
     const {getMovieByIdHook} = useMovies()
     const { data: movieData,isSuccess,isLoading,isError } = getMovieByIdHook(id);
     console.log(movieData)
 
-    const gotoHome = () => {
-        router.push('/')
-    }
     return (
         <>
             {isError && <>Error...</>}
             {isLoading && <>Loading...</>}
             {isSuccess && (
-                <div className="mx-10">
-                    <div className='mt-10 cursor-pointer' onClick={gotoHome}>
-                        <MoveLeft/>
+                <>
+                    <div className='absolute h-[250px] w-full bg-gray-300'></div>
+                    <div className='md:mx-60'>
+                        <div className='relative pt-44'>
+                            <div className='flex'>
+                                <div className='h-52 bg-red-300 w-1/5'>
+                                </div>
+                                <div className='w-4/5'>
+                                    <div className='flex justify-between'>
+                                        <div>
+                                            <div>
+                                                This is title
+                                            </div>
+                                            <div>
+                                                this is star
+                                            </div>
+                                            <div>
+                                                this is date box
+                                            </div>
+                                        </div>
+                                        <div>
+                                            this is button
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            this is synopsis
+                                        </div>
+                                        <div>
+                                            this is detail
+                                        </div>
+                                        <div>
+                                            this is director group
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    {movieData.title}
-                </div>
+                </>
             )}
         </>
     );
